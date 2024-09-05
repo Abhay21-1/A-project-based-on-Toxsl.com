6f1b1eedbd243f6b43dc2ee176aae8b7a409d010
Abhay Kaushal <abuabhay177112@gmail.com>
2024-09-05 13:53:26 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,117 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const counterElement = document.getElementById('co');
+    let currentNumber = 1;
+    
+    function updateCounter() {
+        counterElement.textContent = currentNumber;
+        currentNumber++;
+        if (currentNumber > 12) {
+            clearInterval(intervalId); 
+        }
+    }
+    
+   
+    const intervalId = setInterval(updateCounter, 120);
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const counterElement = document.getElementById('cou');
+    let currentNumber = 288;
+    
+    function updateCounter() {
+        counterElement.textContent = currentNumber;
+        currentNumber++;
+        if (currentNumber > 300) {
+            clearInterval(intervalId); 
+        }
+    }
+    
+   
+    const intervalId = setInterval(updateCounter, 120);
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const counterElement = document.getElementById('coun');
+    let currentNumber = 148;
+    
+    function updateCounter() {
+        counterElement.textContent = currentNumber;
+        currentNumber++;
+        if (currentNumber > 160) {
+            clearInterval(intervalId); 
+        }
+    }
+    
+   
+    const intervalId = setInterval(updateCounter,120);
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const counterElement = document.getElementById('count');
+    let currentNumber = 188;
+    
+    function updateCounter() {
+        counterElement.textContent = currentNumber;
+        currentNumber++;
+        if (currentNumber > 200) {
+            clearInterval(intervalId); 
+        }
+    }
+    
+   
+    const intervalId = setInterval(updateCounter,120);
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const counterElement = document.getElementById('counte');
+    let currentNumber = 1988;
+    
+    function updateCounter() {
+        counterElement.textContent = currentNumber;
+        currentNumber++;
+        if (currentNumber > 2000) {
+            clearInterval(intervalId); 
+        }
+    }
+    
+   
+    const intervalId = setInterval(updateCounter,120);
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const counterElement = document.getElementById('counter');
+    let currentNumber = 4988;
+    
+    function updateCounter() {
+        counterElement.textContent = currentNumber;
+        currentNumber++;
+        if (currentNumber > 5000) {
+            clearInterval(intervalId); 
+        }
+    }
+    
+   
+    const intervalId = setInterval(updateCounter,120);
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const counterElement = document.getElementById('counterr');
+    let currentNumber = 58;
+    
+    function updateCounter() {
+        counterElement.textContent = currentNumber;
+        currentNumber++;
+        if (currentNumber > 70) {
+            clearInterval(intervalId); 
+        }
+    }
+    
+   
+    const intervalId = setInterval(updateCounter,120);
+});
